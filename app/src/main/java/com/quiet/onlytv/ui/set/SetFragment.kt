4dfa81be87e3d8c0d76f8ce6fc9b2aea3d885f07
ludoven <xfxjl@qq.com>
2023-11-10 18:25:24 +0800

    Complete the set tab

--- app/src/main/java/com/quiet/onlytv/ui/set/SetFragment.kt
+++ app/src/main/java/com/quiet/onlytv/ui/set/SetFragment.kt
@@ -1,21 +1,23 @@
 package com.quiet.onlytv.ui.set
 
-import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.ludoven.base.BaseFragment
 import com.ludoven.base.mvp.DefaultPresenter
 import com.quiet.onlytv.MainActivity
-import com.quiet.onlytv.R
 import com.quiet.onlytv.databinding.FragmentSetBinding
+import com.quiet.onlytv.ui.category.CategoryFragment
+import com.quiet.onlytv.ui.favorite.FavoriteFragment
+import com.quiet.onlytv.ui.home.HomeFragment
+import com.quiet.onlytv.ui.movie.MovieFragment
+import com.quiet.onlytv.ui.search.SearchFragment
+import com.quiet.onlytv.ui.show.ShowFragment
+import com.quiet.onlytv.utils.requestFocused
 
 
-class SetFragment : BaseFragment<MainActivity,FragmentSetBinding,DefaultPresenter>() {
-    override fun getPresenter(): DefaultPresenter {
-        return DefaultPresenter()
-    }
+class SetFragment : BaseFragment<MainActivity, FragmentSetBinding, DefaultPresenter>() {
+
 
     override fun initView() {
 
@@ -25,4 +27,22 @@ class SetFragment : BaseFragment<MainActivity,FragmentSetBinding,DefaultPresente
 
     }
 
+
+
+    override fun getPresenter(): DefaultPresenter {
+        return DefaultPresenter()
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_DPAD_UP && binding.setAbout.hasFocus()){
+            getAttachActivity()?.requestFocusTab()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    override fun enableFocusableAndRequestFirstFocused(): Boolean {
+        super.enableFocusableAndRequestFirstFocused()
+        return binding.setAbout.requestFocus()
+    }
 }
